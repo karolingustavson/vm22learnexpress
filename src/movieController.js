03f41812e70dcef3d6fa6fcd9dc0b6bf29dcf662
Karolin Gustavson <karolin.gustavson@tptlive.ee>
2024-02-07 15:40:02 +0200

    added new stuff

--- src/movieController.js
+++ src/movieController.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (req,res) => {
+    if(!fs.existsSync('movies.json')) {
+  let json = {
+    lastId: 0,
+    movies: []
+  };
+  json = JSON.stringify(json);
+  fs.writeFileSync('movies.json', json);
+    }
     let movies = fs.readFileSync('movies.json', 'utf-8');
     movies = JSON.parse(movies);
-   res.render('movies/index.njk',{movies: movies});
+   res.render('movies/index.njk',{movies: movies.movies});
 
 });
 
@@ -16,7 +24,8 @@ router.get('/add', (req, res) => {
  router.post('/add', (req,res) => {
     let movies = fs.readFileSync('movies.json', 'utf-8');
     movies = JSON.parse(movies);
-    movies.push({
+    movies.movies.push({
+        id: movies.lastId++,
         name: req.body.movie,
         year: req.body.year,
         description: req.body.description
