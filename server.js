7b772b8b6d93cbd4da7b19e7680b399ebf77aa51
Karolin Gustavson <karolin.gustavson@tptlive.ee>
2024-02-07 15:07:56 +0200

    add new stuff

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended:true}));
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -22,6 +24,20 @@ app.get('/page2', (req, res) => {
     res.render('form.njk', req.query);
 
   });
+
+  app.get('/circle', (req, res) => {
+    res.render('circle.njk', req.query);
+  });
+  
+  app.post('/circle', (req, res) => {
+    let area = Math.PI * req.body.radius * req.body.radius;
+    let circumference = 2 * Math.PI * req.body.radius;
+    let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+    res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
+  });
+
+const movieController = require('./src/movieController.js');
+app.use('/movies', movieController);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
