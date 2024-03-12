24dc6e81a239e8a510e4ff506841e9783466e6b5
Karolin.Gustavson <Karolin.Gustavson@tptlive.ee>
2024-03-12 21:01:31 +0200

    Su ema

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const nunjucks = require('nunjucks');
+const nunjucks = require('nunjucks')
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+  extended:true
+}));
+
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -12,32 +15,33 @@ nunjucks.configure('views', {
 
 app.get('/', (req, res) => {
   res.render('index.njk');
-//   console.log('somebody visited');
+  //console.log('somebody visited');
 });
 
 app.get('/page2', (req, res) => {
-    res.render('page2.njk');
-  });
+    res.render ('page2.njk');
+});
 
-  app.get('/form', (req, res) => {
-    console.log(req.query);
-    res.render('form.njk', req.query);
+app.get('/form', (req, res) => {
+  console.log(req.query);
+  res.render ('form.njk', req.query);
+});
 
-  });
 
-  app.get('/circle', (req, res) => {
-    res.render('circle.njk', req.query);
-  });
-  
-  app.post('/circle', (req, res) => {
-    let area = Math.PI * req.body.radius * req.body.radius;
-    let circumference = 2 * Math.PI * req.body.radius;
-    let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
-    res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
-  });
+app.get('/circle', (req, res) => {
+  console.log(req.query);
+  res.render ('circle.njk', req.query);
+});
 
-const movieController = require('./src/movieController.js');
-app.use('/movies', movieController);
+app.post('/circle', (req, res) => {
+  let area = Math.PI * req.body.radius * req.body.radius;
+  let diameter = 2 * Math.PI * req.body.radius;
+  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+  res.render('circleAnswer.njk',{r: req.body.radius, a: area, d: diameter, v: volume });
+});
+
+const movieController = require('./src/movieController.js')
+app.use('/movies', movieController)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
